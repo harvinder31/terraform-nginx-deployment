9e00adecd3ddb2031099ae9723554643b3cf742c
harvinder31 <90389630+harvinder31@users.noreply.github.com>
2024-06-03 15:48:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,21 +1,3 @@
-provider "aws" {
-  region = var.aws_region
-}
-
-variable "aws_region" {
-  description = "The AWS region to deploy resources."
-  default     = "us-west-2"
-}
-
-variable "aws_access_key_id" {}
-variable "aws_secret_access_key" {}
-
-provider "aws" {
-  region     = var.aws_region
-  access_key = var.aws_access_key_id
-  secret_key = var.aws_secret_access_key
-}
-
 resource "aws_s3_bucket" "web_app_bucket" {
   bucket = var.s3_bucket_name
 

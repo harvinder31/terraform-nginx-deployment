cfdd2af9361ef7e3eb1369e6512a4fb68d9cf914
harvinder31 <90389630+harvinder31@users.noreply.github.com>
2024-06-03 15:34:28 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,15 @@
+output "s3_bucket_name" {
+  value = aws_s3_bucket.web_app_bucket.bucket
+}
+
+output "ec2_instance_public_ip" {
+  value = aws_instance.web_app.public_ip
+}
+
 output "cloudfront_domain_name" {
-  value = aws_cloudfront_health_distribution.s3_distribution.domain_name
+  value = aws_cloudfront_distribution.web_app_distribution.domain_name
+}
+
+output "route53_domain_name" {
+  value = var.domain_name
 }

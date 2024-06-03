33cdb29e62dd50a18d8632d441185af60951192d
harvinder31 <90389630+harvinder31@users.noreply.github.com>
2024-06-03 15:33:57 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,3 +1,17 @@
 provider "aws" {
-  region  = var.aws_region
+  region = var.aws_region
+}
+
+variable "aws_region" {
+  description = "The AWS region to deploy resources."
+  default     = "us-west-2"
+}
+
+variable "aws_access_key_id" {}
+variable "aws_secret_access_key" {}
+
+provider "aws" {
+  region     = var.aws_region
+  access_key = var.aws_access_key_id
+  secret_key = var.aws_secret_access_key
 }

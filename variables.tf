4dc129a4e7b927a6c4df0e20c0a5a011ac1a7d42
harvinder31 <90389630+harvinder31@users.noreply.github.com>
2024-06-03 15:33:22 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,20 +1,20 @@
-variable "aws_region" {
-  description = "AWS Region for the resources"
-  type        = string
-  default     = "us-west-2"
+variable "aws_access_key_id" {
+  description = "The AWS Access Key ID"
 }
 
-variable "access_key" {
-  description = "AWS Access Key ID"
-  type        = string
-}
-
-variable "secret_key" {
-  description = "AWS Secret Access Key"
-  type        = string
+variable "aws_secret_access_key" {
+  description = "The AWS Secret Access Key"
 }
 
 variable "s3_bucket_name" {
-  description = "S3 Bucket name for storing files"
-  type        = string
+  description = "The S3 bucket name for file storage"
+}
+
+variable "aws_region" {
+  description = "The AWS region"
+  default     = "us-west-2"
+}
+
+variable "domain_name" {
+  description = "The domain name to use for the Route 53 hosted zone."
 }
